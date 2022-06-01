7490ef448bc44b7df57a49c9148654c60459f824
Interrubble <delagarzachris@icloud.com>
2022-06-01 15:29:10 -0700

    updated hydration

--- src/components/pages/header/Header.js
+++ src/components/pages/header/Header.js
@@ -2,10 +2,6 @@ import React from 'react';
 import NavTabs from './NavTabs';
 import './index.css';
 
-const styles = {
-    background: 'linear-gradient(#6B1F80,#E68AFF)'
-}
-
 export default function Header () {
     return (
         <div className="Header">
--- src/components/pages/hydration/Hydration.js
+++ src/components/pages/hydration/Hydration.js
@@ -1,53 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './hydration.css';
-
 export default function Hydration() {
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(document.getElementById("waterAmount").value)
+    }
+    function handleGoal(e) {
+        e.preventDefault();
+        console.log(document.getElementById("waterGoal").value)
+        const waterGoal = document.getElementById("waterGoal").value
+        localStorage.setItem("watergoal", JSON.stringify())
+    }
+    useEffect(()=>{
+        var goal = []
+        goal = JSON.parse(localStorage.getItem("watergoal"));
+        const storedWater = document.getElementById("#storedWater");
+        if (goal!==null){
+            storedWater.push(goal)
+        }
+    })
     return (
         <div className="hydration">
             <h1>Hydration</h1>
+            <h2>GOAL:<card id="storedWater"></card></h2>
             <nav className="hydrateWeek">
-                <li>Saturday</li>
                 <form className="waterForm"><br />
+                    <input type="date" /><br />
                     <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
+                    <input min="0" max="500" type="number" id="waterAmount" name="userWater" /><br />
+                    <button type="submit" onClick={handleSubmit}>Submit</button>
+                    <button type="reset">Reset</button>
                 </form>
-                <li>Sunday</li>
-                <form className="waterForm"><br />
-                    <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <li>Monday</li>
-                <form className="waterForm"><br />
-                    <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <li>Tuesday</li>
-                <form className="waterForm"><br />
-                    <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <li>Wednesday</li>
-                <form className="waterForm"><br />
-                    <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <li>Thursday</li>
-                <form className="waterForm"><br />
-                    <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <li>Friday</li>
-                <form className="waterForm"><br />
-                    <label htmlFor="waterAmount">Input number of ounces drank today:</label><br />
-                    <input type="text" id="waterAmount" name="userWater" /><br />
-                    <input type="submit" value="Submit"></input>
-                </form>
+            </nav>
+            <nav>
+                <h2>GOAL:
+                    <form>
+                        <input min="0" max="500" type='number' id="waterGoal" name="goalWater" />
+                        <button type="submit" onClick={handleGoal}>Submit</button>
+                        <button type="reset">Reset</button>
+                    </form>
+                </h2>
             </nav>
         </div>
     );
