ec726e4281d4f347c29fcf5fcdefa07613dd5bf4
Interrubble <delagarzachris@icloud.com>
2022-06-02 11:54:17 -0700

    fixed and styled fitness and folder capitalization

--- src/components/pages/fitness/Fitness.js
+++ src/components/pages/fitness/Fitness.js
@@ -1,17 +1,116 @@
 import React from 'react';
+import { Card, Button, Form } from 'react-bootstrap';
+import './fitness.css'
 
 export default function Fitness() {
     return (
-        <div>
+        <div className="fitness">
             <h1>Fitness</h1>
-            <p>
-                Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-            </p>
+            <h2>Your Goals</h2>
+            <Form>
+                {[''].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check
+                            type={type}
+                            id={`default-${type}`}
+                            label={`days ${type}`}
+                        />
+                    </div>
+                ))}
+            </Form>
+
+            <Form>
+                {[''].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check
+                            type={type}
+                            id={`default-${type}`}
+                            label={`minutes ${type}`}
+                        />
+                    </div>
+                ))}
+            </Form>
+            <br />
+            <input className="fitnessDate" type="date"></input>
+            <br />
+            <div className="fitnessWeek">
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Monday</h4>
+                        <Card.Text>
+                            <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button className="fitnessBtn" variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+                <br />
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Tuesday</h4>
+                        <Card.Text>
+                        <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+                <br />
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Wednesday</h4>
+                        <Card.Text>
+                        <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+                <br />
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Thursday</h4>
+                        <Card.Text>
+                        <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+                <br />
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Friday</h4>
+                        <Card.Text>
+                        <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+                <br />
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Saturday</h4>
+                        <Card.Text>
+                        <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+                <br />
+                <Card className="fitnessCards" style={{ width: '18rem' }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                    <Card.Body>
+                        <h4>Sunday</h4>
+                        <Card.Text>
+                        <input className="fitnessInput" type="text"></input>
+                        </Card.Text>
+                        <Button variant="primary">Save Daily Changes</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     );
 }
