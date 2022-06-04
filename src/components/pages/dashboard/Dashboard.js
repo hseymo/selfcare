d09a41299cf6afadafca7cffd3ad19b79ab1a21e
Haley Seymour <haleycseymour@comcast.net>
2022-06-03 22:38:42 -0700

    dashboard cards and polishes; code clean up

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -3,9 +3,12 @@ import './dashboard.css';
 import { Link } from 'react-router-dom';
 import API from "../../../utils/API.js"
 import Card from "./card"
+import DashboardRow from './card';
 
 export default function Dashboard({token, weekArray}) {
+  const [userData, setUserData] = useState([])
   const [name, setName] = useState('')
+
   const [goalsData, setGoalsData] = useState({
     fitness_time:'',
     fitness_frequency: '',
@@ -13,15 +16,23 @@ export default function Dashboard({token, weekArray}) {
     hydration_oz: ''
   })
   const [isGoals, setIsGoals] = useState(false)
+
   const [fitnessTime, setFitnessTime] = useState(0)
   const [fitnessCount, setFitnessCount] = useState(0)
-  const [fitnessEmoji, setFitnessEmoji] = useState()
-  // const [sleepEmoji, setSleepEmoji] = useState()
+  const [fitnessEmoji, setFitnessEmoji] = useState([])
+
+  const [sleepEmoji, setSleepEmoji] = useState([])
+  const [sleepWins, setSleepWins] = useState(0)
+
+  const [hydrationEmoji, setHydrationEmoji] = useState([])
+  const [hydrationWins, setHydrationWins] = useState(0)
+
 
   useEffect(() => {
     API.getOneUser(token).then((userData)=>{
+      setUserData(userData)
       setName(userData.first_name);
-      // USERS GOALS_______________________________________________
+// USERS GOALS_______________________________________________
       const { fitness_time, fitness_frequency, sleep_time, hydration_oz } = userData.goal;
       setGoalsData({
         fitness_time: fitness_time,
@@ -33,62 +44,91 @@ export default function Dashboard({token, weekArray}) {
         setIsGoals(true)
       }
       
-      // FITNESS LOGIC__________________________________________
-      // temp holders to set state with
+// FITNESS LOGIC__________________________________________
       const fitnessArray = [];
       let weeklyFitnessTime = 0;
       let weeklyFitnessCount = 0;
       weekArray.map(entry => {
         var response = userData.fitnesses.find(data => data.date === entry);
+        let newObj = {date: entry}
         console.log(response)
         if (response === undefined) {
-          fitnessArray.push('⁇')
-        } else if (response.activity_duration === 0) {
-          fitnessArray.push('❌')
+          newObj.status = 'Not reported'
+          newObj.emoji = '⁇'
         } else {
-          fitnessArray.push('✅');
-          weeklyFitnessTime = weeklyFitnessTime + response.activity_duration;
-          weeklyFitnessCount++;
+          newObj.status = 'Reported'
+          if (response.activity_duration === 0) {
+            newObj.emoji = '❌'
+          } else {
+            newObj.emoji = '✅'
+            weeklyFitnessTime = weeklyFitnessTime + response.activity_duration;
+           weeklyFitnessCount++;
+          }
         }
+        fitnessArray.push(newObj);
       })
-      console.log('fitnessArray: ', fitnessArray)
-      console.log('weeklyFitnessTime: ', weeklyFitnessTime)
-      console.log('weeklyFitnessCount: ', weeklyFitnessCount)
       setFitnessTime(weeklyFitnessTime)
       setFitnessCount(weeklyFitnessCount)
       setFitnessEmoji(fitnessArray)
-      // NEED TO RENDER EMOJI ARRAY TO THE PAGE
+      console.log('fitnessArray: ', fitnessArray)
+      console.log('weeklyFitnessTime: ', weeklyFitnessTime)
+      console.log('weeklyFitnessCount: ', weeklyFitnessCount)
+      console.log('test fitness: ', fitnessArray)
 
       // SLEEP LOGIC_______________________________________________
         const sleepArray = [];
+        let sleepCount = 0;
         weekArray.map(entry => {
           var response = userData.sleep.find(data => data.date === entry);
-          console.log(response)
+          let newObj = {date: entry};
           if (response === undefined) {
-            sleepArray.push('⁇')
+            newObj.status = 'Not reported';
+            newObj.emoji = '⁇';
           } else {
-            sleepArray.push('Reported')
-          }
-          // else if (response.time_asleep >= sleepGoal) {
-          //   sleepArray.push('1')
-          // } else if (response.time_asleep < sleepGoal) {
-          //   sleepArray.push('2')
-          // }
+            newObj.status = 'Reported';
+            if (response.time_asleep >= goalsData.sleep_time) {
+            newObj.emoji = '✅';
+            sleepCount++;
+          } else {
+            newObj.emoji = '❌';
+          } 
+        }
+        sleepArray.push(newObj)
         })
-        console.log(userData.sleep)
+        setSleepWins(sleepCount)
+        setSleepEmoji(sleepArray)
+        console.log('sleepArray: ', sleepArray)
+        console.log('test sleep: ', sleepEmoji)
+        console.log('sleep wins: ', sleepWins)
 
       // HYDRATAION LOGIC__________________________________________
         const hydrationArray = [];
+        let hydrationCount = 0;
+        let dailyHydration = 0;
         weekArray.map(entry => {
           var response = userData.hydrations.find(data => data.date === entry);
+          let newObj = {date: entry}
           console.log(response)
           if (response === undefined) {
-            hydrationArray.push('⁇')
+            newObj.status = 'Not reported'
+            newObj.emoji = '⁇'
           } else {
-            hydrationArray.push('Reported')
+            newObj.status = 'Reported';
+            newObj.oz = response.water_oz;
+            if (response.water_oz >= goalsData.hydration_oz) {
+              newObj.emoji='💦'
+              hydrationCount++;
+            } else {
+              newObj.emoji='💧'
+            }
           }
+          hydrationArray.push(newObj)
         })
-
+        setHydrationEmoji(hydrationArray)
+        setHydrationWins(hydrationCount);
+        console.log('hydrationArray: ', hydrationArray)
+        console.log('test hydration :', hydrationEmoji)
+        console.log('hydration wins: ', hydrationWins)
     })
   }, [token])
 
@@ -100,16 +140,16 @@ export default function Dashboard({token, weekArray}) {
             <>
             <ul className='goalsList'>
               { goalsData.fitness_time != 0 && (
-              <li className='goalsLi'>You said you wanted to exercise {goalsData.fitness_time} minutes per week. </li>
+              <li className='goalsLi'>Your exercise time goal is {goalsData.fitness_time} minutes per week. </li>
               )}
               { goalsData.fitness_frequency != 0 && (
-              <li className='goalsLi'>You said you wanted to exercise {goalsData.fitness_frequency} days per week. </li>
+              <li className='goalsLi'>Your exercise frequency goal is {goalsData.fitness_frequency} days per week. </li>
               )}
               {goalsData.sleep_time != 0 && (
-              <li className='goalsLi'>You said you wanted to sleep {goalsData.sleep_time} hours per night.</li>
+              <li className='goalsLi'>Your nightly sleep goal is {goalsData.sleep_time} hours.</li>
               )}
               { goalsData.hydration_oz != 0 && (
-              <li className='goalsLi'>You said you wanted to drink {goalsData.hydration_oz} oz of water per day.</li>
+              <li className='goalsLi'>Your daily water intake goal is {goalsData.hydration_oz} oz.</li>
               )}
             </ul>
             <button className='goalsLink' onClick={(e) => {window.location.href = "/profile"}}>Update my goals</button>
@@ -129,32 +169,28 @@ export default function Dashboard({token, weekArray}) {
           <th>Saturday <br/> {weekArray[5]}</th>
           <th>Sunday <br/> {weekArray[6]}</th>
         </tr>
-        <tr>
-          <td className="rowHeader"><Link to='/fitness'>Did you workout today?</Link></td>
-          <td> ❌ </td>
-          <td> ✅ </td>
-          <td> ⁇ </td>
-          <td> ✅ </td>
-          <td> ✅ </td>
-          <td> ❌ </td>
-          <td> ❌ </td>
-        </tr>
+        <DashboardRow 
+            name='fitness' 
+            link='/fitness' 
+            results={fitnessEmoji}/>
       </table>
       
-      <h2>So far this week:</h2>
-      <h3> You have reported {fitnessTime} minutes of exercise.</h3>
+      <h4>So far this week:</h4>
+      <ul>
+      <li className="compLi"> You have reported {fitnessTime} minutes of exercise.</li>
       {/* NOT PROTECTING AGAINST 0 GOAL  */}
       {(goalsData.fitness_time != 0 && fitnessTime >= goalsData.fitness_time) ? (
-        <h3>You are {fitnessTime - goalsData.fitness_time} minutes above your goal of {goalsData.fitness_time}!</h3>
+        <li className='compLi'>You are {fitnessTime - goalsData.fitness_time} minutes above your goal of {goalsData.fitness_time}!</li>
        ) : (
-        <h3>You are {goalsData.fitness_time - fitnessTime} minutes below your goal of {goalsData.fitness_time}.</h3>
+        <li className='compLi'>You are {goalsData.fitness_time - fitnessTime} minutes below your goal of {goalsData.fitness_time}.</li>
       )}
-      <h3>You reported {fitnessCount} days of exercise!</h3> 
+      <li className='compLi'>You reported {fitnessCount} days of exercise!</li> 
       {(fitnessCount >= goalsData.fitness_frequency) ? 
-        <h3>You met your goal of {goalsData.fitness_frequency}!</h3>
+        <li className='compLi'>You met your goal of {goalsData.fitness_frequency}!</li>
           : 
-        <h3>You are not at your goal of {goalsData.fitness_frequency} days per week.</h3>
+        <li className='compLi'>You are not at your goal of {goalsData.fitness_frequency} days per week.</li>
       }
+      </ul>
       <h2>Sleep and Hydration</h2>
                   <table>
         <tr className="dayHeaders">
@@ -167,30 +203,16 @@ export default function Dashboard({token, weekArray}) {
           <th>Saturday <br/> {weekArray[5]}</th>
           <th>Sunday <br/> {weekArray[6]}</th>
         </tr>
-        <tr>
-          <td className="rowHeader"><Link to='/sleep'>Did you meet your sleep goal today?</Link></td>
-          {/* {sleepData.forEach((sleep) => (
-            console.log('sleepy')
-          ))} */}
-          <td> ✅ </td>
-          <td> ✅ </td>
-          <td> ✅ </td>
-          <td> ❌ </td>
-          <td> ❌ </td>
-          <td> ✅ </td>
-          <td> ❌ </td>
-        </tr>
-        <tr>
-          <td className="rowHeader"><Link to='/hydration'>Did you meet your hydration goal today?</Link></td>
-          <td> 💧 </td>
-          <td> 💦 </td>
-          <td> 💧 </td>
-          <td> 💦 </td>
-          <td> 💦 </td>
-          <td> 💦 </td>
-          <td> 💧 </td>
-        </tr>
+          <DashboardRow 
+            name='sleep' 
+            link='/sleep' 
+            results={sleepEmoji}/>
+          <DashboardRow 
+            name='hydration' 
+            link='/hydration' 
+            results={hydrationEmoji}/>
         </table>
+
       <h3>Click on a category to see more!</h3>
         </div>
     );
--- src/components/pages/dashboard/card.js
+++ src/components/pages/dashboard/card.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function DashboardCard(props) {
+export default function DashboardRow(props) {
 
     return (
-        <div className='dashboardCards'>
-            <h3>Activity completed: {props.fitness}</h3>
-        </div>
+        <tr>
+            <td className="rowHeader">
+                <Link to={props.link}>
+                    { props.name === 'fitness' ? (
+                        "Did you workout today?"
+                    ) : (
+                    `Did you meet your ${props.name} goal today?`
+                    )
+                }
+                </Link>
+            </td>
+            {props.results.map((result) => 
+            // link each box to respective update/add info 
+                <td date={result.date}>{result.emoji}</td>
+            )}
+        </tr>
     )
 }
