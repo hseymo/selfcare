c72e0ed9d637616beacf5c4d38ce9abfbe544d24
Interrubble <delagarzachris@icloud.com>
2022-06-08 21:31:44 -0700

    working on hydration animation

--- src/components/pages/hydration/Progress.js
+++ src/components/pages/hydration/Progress.js
@@ -1,28 +1,36 @@
 import React, { useState } from 'react'
-import { Spring, useSpring, animated, config } from 'react-spring'
+import { SpringValue, AnimatedValue, Spring, useSpring, animated, config } from 'react-spring'
+import useMeasure from 'react-use-measure'
+import './progress.css'
 
 export default function Progress(props) {
-    const [progress, setProgress] = useState({});
+    // const [progress, setProgress] = useState({});
     // setProgress({
     //     goal: props.goal,
     //     amount: props.amount,
     // });
 
-    const anim = useSpring({ width: progress, config: config.slow });
+    // const anim = useSpring({ width: progress, config: config.slow });
+    console.log(props)
+    const [open, toggle] = useState(false);
+    const [ref, {width}] = useMeasure();
+    const anima = useSpring({ width: open ? width : 0 })
+
     return (
-        <>
-        <h2>{props.amount}</h2>
-        <Spring
-        from={{ opacity: 0, width: 0 }}
-        to={{ opacity: 1, width: `${props.amount}` }}
-        // config={{ duration: 2000 }}
-        >
-            {anim => (
-                <animated.div style={anim}>
-                    
+        <div className='container'>
+            <div ref={ref} className='main' onClick={() => toggle(!open)}>
+                <animated.div className='fill' style={{
+                    position: `absolute`,
+                    top: 50,
+                    left: 0,
+                    width: `${props.amount/props.goal}%`,
+                    height: '100%',
+                    background: 'hotpink',
+                }} />
+                <animated.div className='content'>
+                    {anima.width.to(x=>x.toFixed())}
                 </animated.div>
-            )}
-        </Spring>
-        </>
+            </div>
+        </div>
     )
 }
