618e5324bbcd1a4ce4e5e3022c79fd6a02bba514
Interrubble <delagarzachris@icloud.com>
2022-06-09 11:36:36 -0700

    made bottle animation

--- src/components/pages/hydration/Hydration.js
+++ src/components/pages/hydration/Hydration.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import ReactDOM from 'react-dom/client'
 import './hydration.css';
 import { Link } from 'react-router-dom';
 import API from "../../../utils/API.js"
@@ -17,7 +18,7 @@ export default function Hydration({ token, weekArray, goalObj, isLoggedIn }) {
     })
     const [updateReq, setUpdateReq] = useState('');
     const [existingItem, setExistingItem] = useState('');
-        const [anotherDate, setAnotherDate] = useState('');
+    const [anotherDate, setAnotherDate] = useState('');
     const [anotherWeek, setAnotherWeek] = useState('');
     const [error, setError] = useState('');
 
@@ -28,12 +29,10 @@ export default function Hydration({ token, weekArray, goalObj, isLoggedIn }) {
             const hydrationArray = [];
             weekArray.map(entry => {
                 var response = userData.find(data => data.date === entry);
-let dateFormat = entry.slice(5) + "-" + entry.slice(0,4);
-                
+                let dateFormat = entry.slice(5) + "-" + entry.slice(0, 4);
+
                 let newObj = { date: dateFormat }
                 console.log(response)
-
-                let newObj = { date: entry }
 
                 if (response === undefined) {
                     newObj.status = 'Not Reported';
@@ -59,7 +58,7 @@ let dateFormat = entry.slice(5) + "-" + entry.slice(0,4);
         })
     }, [token, updateReq])
 
-    
+
 
     useEffect(() => {
         API.getOneUserHydration(token, hydrationFormObject.date).then((response) => {
@@ -112,19 +111,19 @@ let dateFormat = entry.slice(5) + "-" + entry.slice(0,4);
         setUpdateReq(false)
     })
 
-        const reqAnotherWeek = useCallback( async(e) => {
+    const reqAnotherWeek = useCallback(async (e) => {
         e.preventDefault();
-            if (anotherDate) {
+        if (anotherDate) {
             let anotherWeek = [];
             for (let i = 1; i < 8; i++) {
                 let thisDay = moment(anotherDate).day(i).format("YYYY-MM-DD");
                 anotherWeek.push(thisDay)
-              }
+            }
 
             const anotherHydrationArray = [];
             anotherWeek.map(entry => {
                 var response = Data.find(data => data.date === entry);
-                let dateFormat = entry.slice(5) + "-" + entry.slice(0,4);
+                let dateFormat = entry.slice(5) + "-" + entry.slice(0, 4);
 
                 let newObj = { date: dateFormat }
 
@@ -150,89 +149,89 @@ let dateFormat = entry.slice(5) + "-" + entry.slice(0,4);
             setError('Please choose a valid date.')
         }
     })
-    
+
     return (
-    <div className="hydration">
-        {!isLoggedIn ? (
-            <h2><Link class="link-light" to='/login'>Login</Link></h2>
+        <div className="hydration">
+            {!isLoggedIn ? (
+                <h2><Link class="link-light" to='/login'>Login</Link></h2>
             ) : (
                 <>
-                <h1>Hydration</h1>
-                <h2>Your Goals</h2>
-                {goalObj.hydration_oz != 0 && (
-                    <h4 className=''>Your daily water intake goal is {goalObj.hydration_oz} oz.</h4>
-                )}
-                <h2>Report Water Intake</h2>
-                <form className="waterForm">
-                    <label htmlFor="waterDate">
-                        Choose date:
-                    </label>
-                    <input
-                        value={hydrationFormObject.date}
-                        type="date"
-                        id="waterDate"
-                        name="waterDate"
-                        onChange={(e) => setHydrationFormObject({ ...hydrationFormObject, date: e.target.value })}
-                    />
-                    <label htmlFor="waterAmount">
-                        How many ounces did you drink?
-                    </label>
-                    <input
-                        value={hydrationFormObject.water_oz}
-                        min="0"
-                        max="1000"
-                        type="number"
-                        id="waterAmount"
-                        name="waterAmount"
-                        onChange={(e) => setHydrationFormObject({ ...hydrationFormObject, water_oz: e.target.value })}
-                    />
-                    <br />
-                    {(existingItem == true) ? (
-                        <>
-                            <button type="button" className="hydroBtn"
-                                onClick={sendUpdate}>Update</button>
-                            <button type="button" className="hydroBtn"
-                            onClick={sendDelete}>Delete</button>
-                        </>
+                    <h1>Hydration</h1>
+                    <h2>Your Goals</h2>
+                    {goalObj.hydration_oz != 0 && (
+                        <h4 className=''>Your daily water intake goal is {goalObj.hydration_oz} oz.</h4>
+                    )}
+                    <h2>Report Water Intake</h2>
+                    <form className="waterForm">
+                        <label htmlFor="waterDate">
+                            Choose date:
+                        </label>
+                        <input
+                            value={hydrationFormObject.date}
+                            type="date"
+                            id="waterDate"
+                            name="waterDate"
+                            onChange={(e) => setHydrationFormObject({ ...hydrationFormObject, date: e.target.value })}
+                        />
+                        <label htmlFor="waterAmount">
+                            How many ounces did you drink?
+                        </label>
+                        <input
+                            value={hydrationFormObject.water_oz}
+                            min="0"
+                            max="1000"
+                            type="number"
+                            id="waterAmount"
+                            name="waterAmount"
+                            onChange={(e) => setHydrationFormObject({ ...hydrationFormObject, water_oz: e.target.value })}
+                        />
+                        <br />
+                        {(existingItem == true) ? (
+                            <>
+                                <button type="button" className="hydroBtn"
+                                    onClick={sendUpdate}>Update</button>
+                                <button type="button" className="hydroBtn"
+                                    onClick={sendDelete}>Delete</button>
+                            </>
                         ) : (
                             <button className="hydroBtn" type="button" onClick={sendCreate}>Submit</button>
                         )}
-                </form>
-<Progress className='progressBox' goal={goalObj.hydration_oz} amount={hydrationFormObject.water_oz}/>
+                    </form>
+                    <Progress className='progressBox' goal={goalObj.hydration_oz} amount={hydrationFormObject.water_oz} />
 
-            <h2>This week's hydration reporting: </h2>
-            <HydrationCard
-                name='hydrationCard'
-                results={thisWeek}
-                goal={goalObj.hydration_oz}
-            />
-                      <div className='anotherWeekSection'> 
-            <h2>View another week's hydration reporting:</h2>
-            <form className='chooseDate'>
-                <label htmlFor='anotherDate'>Date</label>
-                <input
-                    value={anotherDate}
-                    type="date"
-                    name="anotherDate"
-                    onChange={(e) => setAnotherDate(e.target.value)}
-                />
-        {error && (
-            <div>
-              <p className="error">{error}
-              </p>
-            </div>
-          )}
-                <button className="hydroBtn" type="button" onClick={reqAnotherWeek}>Submit</button>
-            </form>
-            { anotherWeek ? (
-            <HydrationCard
-                results={anotherWeek} />
-            ) : (
-                <></>
-                )}
-            </div>  
-        </>
+                    <h2>This week's hydration reporting: </h2>
+                    <HydrationCard
+                        name='hydrationCard'
+                        results={thisWeek}
+                        goal={goalObj.hydration_oz}
+                    />
+                    <div className='anotherWeekSection'>
+                        <h2>View another week's hydration reporting:</h2>
+                        <form className='chooseDate'>
+                            <label htmlFor='anotherDate'>Date</label>
+                            <input
+                                value={anotherDate}
+                                type="date"
+                                name="anotherDate"
+                                onChange={(e) => setAnotherDate(e.target.value)}
+                            />
+                            {error && (
+                                <div>
+                                    <p className="error">{error}
+                                    </p>
+                                </div>
+                            )}
+                            <button className="hydroBtn" type="button" onClick={reqAnotherWeek}>Submit</button>
+                        </form>
+                        {anotherWeek ? (
+                            <HydrationCard
+                                results={anotherWeek} />
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                </>
             )}
-    </div>
+        </div>
     );
 }
