28839f917ac825c952ade5bcd32f1fcf64d464cd
Interrubble <delagarzachris@icloud.com>
2022-06-10 10:17:34 -0700

    removed commented out code/redundant code

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,7 +1,6 @@
 import React, { useState, htmlFor } from 'react';
 import "./login.css";
 import { Link } from 'react-router-dom';
-// import { validateEmail, checkPassword } from '../../../utils/helpers';
 
 export default function Login(props) {
   const [signupData, setSignupData] = useState({
