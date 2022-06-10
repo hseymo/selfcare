e61cc73bebfbcc23ea3dad738b3b11c03cc25a54
Interrubble <delagarzachris@icloud.com>
2022-06-10 12:45:32 -0700

    navbar shenanigans

--- src/components/pages/header/NavTabs.js
+++ src/components/pages/header/NavTabs.js
@@ -1,54 +1,96 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi'
 import './navtabs.css'
 
-function NavTabs({isLoggedIn, userId, logout}) {
+
+function NavTabs({ isLoggedIn, userId, logout }) {
+    const [navbarOpen, setNavbarOpen] = useState(false)
+    const handleToggle = () => {
+        setNavbarOpen(prev => !prev)
+    }
+    const closeMenu = () => {
+        setNavbarOpen(true)
+    }
+
     return (
-        
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-            <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" >
-            <Link className="pageLink" to='/dashboard'>Dashboard</Link>
-            <div></div>
-            <Link className="pageLink" to='/' onClick={logout}>Logout</Link> 
+
+        <nav className='navBar'>
+            <button onClick={handleToggle}>
+                {navbarOpen ? (
+                    <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+                ) : (
+                    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                )}
+                {isLoggedIn ? (
+                    // <button onClick={closeMenu}>
+                        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/dashboard' onClick={closeMenu}>Dashboard</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/profile' onClick={closeMenu}>Profile</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/fitness' onClick={closeMenu}>Fitness</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/sleep' onClick={closeMenu}>Sleep</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/hydration' onClick={closeMenu}>Hydration</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/mindfulness' onClick={closeMenu}>Mindfulness</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className="pageLink" to='/' onClick={logout}>Logout</Link>
+                            </li>
+                        </ul>
+                    // </button>
+                ) : (
+                    <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+                        <li className='nav-item'>
+                            <Link className="pageLink" to='/login'>Login</Link>
+                        </li>
+                    </ul>
+                )}
             </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-            <ul className="nav nav-tabs"> 
-        {!isLoggedIn ? (
-            <li className="nav-item">
-                <Link className="pageLink" to='/login'>Login</Link>
-            </li>
-        ) : (
-            <>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/dashboard'>Dashboard</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/profile'>Profile</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/fitness'>Fitness</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/sleep'>Sleep</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/hydration'>Hydration</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/mindfulness'>Mindfulness</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="pageLink" to='/' onClick={logout}>Logout</Link>
-                </li>
-                    </>
-        )}
-            </ul>
-            </ul> 
-            </div> 
-            </div>
-            </nav>
+            {/* <ul class="navbar-nav mr-auto">
+                <ul className="nav nav-tabs">
+                    {!isLoggedIn ? (
+                        <li className="nav-item">
+                            <Link className="pageLink" to='/login'>Login</Link>
+                        </li>
+                    ) : (
+                        <>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/dashboard'>Dashboard</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/profile'>Profile</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/fitness'>Fitness</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/sleep'>Sleep</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/hydration'>Hydration</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/mindfulness'>Mindfulness</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="pageLink" to='/' onClick={logout}>Logout</Link>
+                            </li>
+                        </>
+                    )}
+                </ul>
+            </ul> */}
+        </nav>
     );
 }
 
